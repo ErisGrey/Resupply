c8072a9b31fb836b4da9f958808faba868a79a26
Thanh.MT200590@sis.hust.edu.vn <Thanh.MT200590@sis.hust.edu.vn>
2024-10-10 21:00:52 +0700

    10/10/24

---
+++ instance.h
@@ -0,0 +1,218 @@
+#pragma once
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+
+struct Point {
+    float x;
+    float y;
+    int release_date;
+};
+
+class Instance
+{
+protected:
+    Instance(const std::string input, const std::string param)
+    {
+        read_input(input);
+        read_param_input(param);
+        initialize();
+    }
+    static Instance* singleton_;
+    std::vector < Point > Points;
+    
+public:
+    int num_nodes;
+    int v_truck;
+    int v_drone;
+    int delta;
+    int drone_capacity;
+    std::vector<float> x;
+    std::vector<float> y;
+    std::vector<int> release_time;
+    std::vector< std::vector<float> >time_drone;
+    std::vector< std::vector<float> >time_truck;
+    std::vector< std::vector<float> > dist_truck;
+    std::vector< std::vector<float> > dist_drone;
+    std::vector<int> truckonly;
+    std::vector<int> freemode;
+    Instance(Instance& other) = delete;
+    void operator=(const Instance&) = delete;
+    static Instance* getInstance(const std::string input, const std::string param);
+    template <class Container>
+    void split(const std::string& str, Container& cont, char delim);
+    int getNumNode() { return num_nodes; }
+    void read_input(const std::string input)
+    {
+        std::ifstream myFile(input);
+
+        if (!myFile.is_open())
+            std::cerr << "File input is missing !!";
+
+        num_nodes = 0;
+        std::string line;
+        std::vector<std::string> numbers;
+
+        //skip header
+        std::getline(myFile, line);
+        //std::cout << line << '\n';
+
+        while (std::getline(myFile, line))
+        {
+            num_nodes++;
+            split(line, numbers, '\t');
+            /*for (auto i : numbers)
+            {
+                std::cout << i << " ";
+            }
+            std::cout << '\n';
+            std::cout << numbers.size() << '\n';*/
+            x.push_back(stof(numbers[1]));
+            y.push_back(stof(numbers[2]));
+            release_time.push_back(stoi(numbers[3]));
+
+        }
+
+        // depot n+1
+        x.push_back(x[0]);
+        y.push_back(y[0]);
+        release_time.push_back(release_time[0]);
+        num_nodes++;
+
+        std::cerr << x[0] << ' ' << y[0] << ' ' << release_time[0] << '\n';
+        std::cout << "num_node = " << num_nodes << '\n';
+    }
+    void read_param_input(const std::string param)
+    {
+        std::ifstream myFile(param);
+
+        if (!myFile.is_open())
+            std::cerr << "File input is missing !!";
+
+        std::string line;
+        std::vector<std::string> numbers;
+
+        
+        std::getline(myFile, line);
+        split(line, numbers, ',');
+        v_truck = stoi(numbers[1]);
+
+        std::getline(myFile, line);
+        split(line, numbers, ',');
+        v_drone = stoi(numbers[1]);
+
+        std::getline(myFile, line);
+        split(line, numbers, ',');
+        delta = stoi(numbers[1]);
+
+        std::getline(myFile, line);
+        split(line, numbers, ',');
+        drone_capacity = stoi(numbers[1]);
+    }
+    void initialize()
+    {
+        time_drone.resize(num_nodes);
+        time_truck.resize(num_nodes);
+        for (int i = 0; i < num_nodes; i++)
+        {
+            time_drone[i].resize(num_nodes);
+            time_truck[i].resize(num_nodes);
+        }
+
+        dist_drone.resize(num_nodes);
+        dist_truck.resize(num_nodes);
+        for (int i = 0; i < num_nodes; i++)
+        {
+            dist_drone[i].resize(num_nodes);
+            dist_truck[i].resize(num_nodes);
+        }
+
+        for (int i = 0; i < num_nodes; i++) {
+            for (int j = 0; j < num_nodes; j++) {
+                float euc_d = pow(pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2), 0.5);
+                float man_d = abs(x[i] - x[j]) + abs(y[i] - y[j]);
+
+                dist_drone[i][j] = euc_d;
+                dist_truck[i][j] = man_d;
+
+                
+                time_drone[i][j] = (euc_d / v_drone) * 60; //unit minute
+                time_truck[i][j] = (man_d / v_truck) * 60; //unit minute
+            }
+        }
+
+        //for (int i = 0; i < num_nodes; i++) {
+        //    for (int j = 0; j < num_nodes; j++) {
+        //        std::cout << "time_truck[" << i << "][" << j << "] = " << time_truck[i][j] << '\n';
+
+        //        //            assert(time_drone[i][j] * 2 <= maxTdrone);
+        //        //            assert(time_truck[i][j] * 2 <= maxTtruck);
+        //    }
+        //}
+
+        truckonly = {};
+        freemode = {};
+        for (int i = 1; i < num_nodes - 1; ++i) {
+            if (time_drone[0][i] >= 45) 
+                truckonly.push_back(i);
+            else 
+                freemode.push_back(i);
+            
+        }
+
+        for (auto i : truckonly)
+        {
+            std::cout << i << " ";
+        }
+        std::cout << '\n';
+
+    }
+
+    double tdrone_all(int i)
+    {
+        return 2 * time_drone[0][i];
+    }
+
+    /*int getNbJob() { return nb_job; }
+    int getNbMachine() { return nb_machine; }
+    int getNbConflict() { return nb_conflict; }
+    int getHorizon() { return horizon; }
+    int getSumPt() {
+        int sum = 0;
+        for (int i = 0; i < nb_job; i++)
+            sum += jobs[i].processing_time;
+        return sum;
+    }
+    std::vector < Job > getJobsList() { return jobs; }
+    Job getJob(int i) { return jobs[i]; }
+    std::vector< std::pair< int, int > > getConflictList() {
+        return conflicts;
+    }*/
+};
+
+
+Instance* Instance::singleton_ = nullptr;;
+Instance* Instance::getInstance(const std::string input = "", const std::string param = "")
+{
+    if (singleton_ == nullptr) {
+        singleton_ = new Instance(input, param);
+    }
+    return singleton_;
+}
+
+template<class Container>
+void Instance::split(const std::string& str, Container& cont, char delim)
+{
+    cont.clear();
+    std::stringstream ss(str);
+    std::string token;
+    while (std::getline(ss, token, delim)) {
+        if (token != "")
+            cont.push_back(token);
+    }
+}
---
+++ pdstsp_dr.h
@@ -0,0 +1,654 @@
+﻿#pragma once
+#include "Model.h"
+#include "Params.h"
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+
+typedef IloArray<IloArray<IloNumVarArray>> NumVar3D;
+typedef IloArray<IloArray<IloNumArray>> Num3D;
+typedef IloArray<IloNumVarArray> NumVar2D;
+typedef IloArray<IloNumArray> Num2D;
+
+typedef std::unordered_map<int, int> MapSol2D;
+typedef std::vector<std::unordered_map<int, int>> MapSol3D;
+typedef std::vector<std::tuple<int, int>> Sol2D;
+typedef std::vector<std::tuple<int, int, int>> Sol3D;
+class PDSTSP_DR : public Model {
+private:
+    IloEnv env;
+    NumVar2D x;
+    NumVar2D r;
+    IloNumVarArray z;
+    IloNumVarArray u;
+    NumVar2D y;
+    IloNumVarArray T;
+    IloNumVarArray s;
+    IloNumVarArray e;
+    IloNumVar T_max;
+    IloNumVar Td;
+    int n;
+    std::vector<int> N;
+    std::vector<int> D;
+    std::vector<int> N0;
+    std::vector<int> Ne;
+    std::vector<int> D0;
+    std::vector<int> De;
+    std::vector<int> Na;
+public:
+
+    PDSTSP_DR() {
+        Instance* instance = Instance::getInstance();
+        n = instance->num_nodes;
+        std::cout << "num_nodes = " << n << '\n';
+        for (int i = 1; i < n - 1; i++)
+        {
+            N.push_back(i);
+            N0.push_back(i);
+            Ne.push_back(i);
+            Na.push_back(i);
+        }
+        N0.push_back(0);
+        Ne.push_back(n - 1);
+        Na.push_back(0);
+        Na.push_back(n - 1);
+
+
+        for (int i : instance->freemode)
+        {
+            D.push_back(i);
+            D0.push_back(i);
+            De.push_back(i);
+        }
+        D0.push_back(0);
+        De.push_back(n - 1);
+
+        /*std::cout << "N: ";
+        for (auto i : N)
+            std::cout << i << " ";
+        std::cout << '\n';
+
+        std::cout << "N0: ";
+        for (auto i : N0)
+            std::cout << i << " ";
+        std::cout << '\n';
+
+        std::cout << "Ne: ";
+        for (auto i : Ne)
+            std::cout << i << " ";
+        std::cout << '\n';
+
+        std::cout << "Na: ";
+        for (auto i : Na)
+            std::cout << i << " ";
+        std::cout << '\n';
+
+        std::cout << "D: ";
+        for (auto i : D)
+            std::cout << i << " ";
+        std::cout << '\n';
+
+        std::cout << "D0: ";
+        for (auto i : D0)
+            std::cout << i << " ";
+        std::cout << '\n';
+
+        std::cout << "De: ";
+        for (auto i : De)
+            std::cout << i << " ";
+        std::cout << '\n';*/
+
+        env = IloEnv();
+        x = NumVar2D(env, n);
+        y = NumVar2D(env, n);
+        r = NumVar2D(env, n);
+        z = IloNumVarArray(env, n);
+        u = IloNumVarArray(env, n);
+        T = IloNumVarArray(env, n);
+        s = IloNumVarArray(env, n);
+        e = IloNumVarArray(env, n);
+        T_max = IloNumVar(env);
+        Td = IloNumVar(env);
+
+        std::stringstream name;
+        // x_ij
+        for (int i : N0)
+        {
+            x[i] = IloNumVarArray(env, n);
+            for (int j : Ne)
+            {
+                if (i == j)
+                    continue;
+                name << "x." << i << "." << j;
+                x[i][j] = IloNumVar(env, 0, 1, ILOINT, name.str().c_str());
+                name.str("");
+            }
+        }
+
+        //y_ij
+        for (int i : D0)
+        {
+            y[i] = IloNumVarArray(env, n);
+            for (int j : N)
+            {
+                name << "y." << i << "." << j;
+                y[i][j] = IloNumVar(env, 0, 1, ILOINT, name.str().c_str());
+                name.str("");
+            }
+        }
+
+        //r_ij
+        for (int i : D0)
+        {
+            r[i] = IloNumVarArray(env, n);
+            for (int j : De)
+            {
+                if (i == j)
+                    continue;
+                name << "r." << i << "." << j;
+                r[i][j] = IloNumVar(env, 0, 1, ILOINT, name.str().c_str());
+                name.str("");
+            }
+        }
+        //z_i
+        for (int i : N)
+        {
+            name << "z." << i;
+            z[i] = IloNumVar(env, 0, 1, ILOINT, name.str().c_str());
+            name.str("");
+        }
+
+
+        //u_i
+        for (int i : D0)
+        {
+            name << "u." << i;
+            u[i] = IloNumVar(env, 0, 1, ILOINT, name.str().c_str());
+            name.str("");
+        }
+
+        //T_i
+        for (int i : Na)
+        {
+            name << "T." << i;
+            T[i] = IloNumVar(env, 0, IloInfinity, ILOFLOAT, name.str().c_str());
+            name.str("");
+        }
+
+        //s_i
+        for (int i : D)
+        {
+            name << "s." << i;
+            s[i] = IloNumVar(env, 0, IloInfinity, ILOFLOAT, name.str().c_str());
+            name.str("");
+        }
+
+        //e_i
+        for (int i : D)
+        {
+            name << "e." << i;
+            e[i] = IloNumVar(env, 0, IloInfinity, ILOFLOAT, name.str().c_str());
+            name.str("");
+        }
+
+        //T_max
+        name << "T_max";
+        T_max = IloNumVar(env, 0, IloInfinity, ILOFLOAT, name.str().c_str());
+        name.str("");
+
+        //Td
+        name << "Td";
+        Td = IloNumVar(env, 0, IloInfinity, ILOFLOAT, name.str().c_str());
+        name.str("");
+
+    }
+    IloModel createModel() {
+        Instance* instance = Instance::getInstance();
+        Param* param = Param::getInstance();
+        IloModel model(env);
+
+        //Anh swuar chỗ này nhá anh :)))
+        
+        IloExpr objExpr(env);
+        objExpr += T_max;
+        model.add(IloMinimize(env, objExpr));
+
+        //Makespan of whole tour
+        model.add(T_max >= T[n - 1]);
+        model.add(T_max >= Td);
+
+        //Truck routing contraints
+        IloExpr sum_s(env);
+        for (int j : N)
+        {
+            sum_s += x[0][j];
+        }
+        model.add(sum_s <= 1);
+
+        IloExpr sum_e(env);
+        for (int j : N)
+        {
+            sum_e += x[j][n - 1];
+        }
+        model.add(sum_e <= 1);
+
+        model.add(sum_s == sum_e);
+
+        for (int j : N)
+        {
+            IloExpr sum_in(env);
+            for (int i : N0)
+            {
+                if (i == j)
+                    continue;
+                sum_in += x[i][j];
+            }
+            
+            IloExpr sum_out(env);
+            for (int i : Ne)
+            {
+                if (i == j)
+                    continue;
+                sum_out += x[j][i];
+            }
+
+            model.add(sum_in == sum_out);
+            model.add(sum_in == z[j]);
+        }
+
+        //Drone routing consrtaints
+        for (int i : D)
+        {
+            model.add(u[i] <= u[0]);
+        }
+
+        IloExpr drin(env);
+        for (int j : D)
+        {
+            drin += r[0][j];
+        }
+        model.add(drin == 1);
+
+        IloExpr drout(env);
+        for (int i : D)
+        {
+            drout += r[i][n - 1];
+        }
+        model.add(drout == 1);
+
+        model.add(drin == drout);
+        drin.end();
+        drout.end();
+        for (int j : D)
+        {
+            model.add(r[0][j] <= u[j] + (1 - z[j]));
+            model.add(u[j] <= z[j]);
+        }
+
+        for (int j : D)
+        {
+            IloExpr sum_drone_degree(env);
+            for (int i : D0)
+            {
+                if (i == j)
+                    continue;
+                sum_drone_degree += r[i][j];
+            }
+            model.add(sum_drone_degree == u[j] + 1 - z[j]);
+            sum_drone_degree.end();
+        }
+
+        for (int j : D)
+        {
+            IloExpr sum_drone_in(env);
+            for (int i : D0)
+            {
+                if (i == j)
+                    continue;
+                sum_drone_in += r[i][j];
+            }
+
+            IloExpr sum_drone_out(env);
+            for (int i : De)
+            {
+                if (i == j)
+                    continue;
+                sum_drone_out += r[j][i];
+            }
+
+            model.add(sum_drone_in == sum_drone_out);
+        }
+
+        for (int i : D)
+        {
+            IloExpr sum_drone_degree(env);
+            for (int j : De)
+            {
+                if (i == j)
+                    continue;
+                sum_drone_degree += r[i][j];
+            }
+            model.add(sum_drone_degree == u[i] + 1 - z[i]);
+            sum_drone_degree.end();
+        }
+
+        //Drone capacity constraint
+        for (int i : D)
+        {
+            IloExpr sum_order(env);
+            for (int j : N)
+            {
+                sum_order += y[i][j] * 1; //1 is weight of order, all order have weight 1
+            }
+            model.add(sum_order <= instance->drone_capacity * u[i]);
+            sum_order.end();
+
+            model.add((1 - z[i]) * 1 <= instance->drone_capacity);
+        }
+        double M = 9999;
+        //Constraint on loading the orders onto the truck
+        for (int j : N)
+        {
+            IloExpr load_order(env);
+            for (int i : D0)
+            {
+                load_order += y[i][j];
+            }
+            model.add(load_order == z[j]);
+            load_order.end();
+        }
+
+        for (int j : D)
+        {
+            IloExpr load_order(env);
+            for (int i : N)
+            {
+                load_order += y[j][i];
+            }
+
+            model.add(load_order <= M * z[j]);
+        }
+
+        
+        //Synchronization and timing constraints
+        for (int j : N)
+        {
+            model.add(T[j] >= (instance->release_time[j] + std::min(instance->time_drone[0][j], instance->time_truck[0][j])) * z[j]);
+        }
+
+        for (int i : D0)
+        {
+            for (int j : N)
+            {
+                if (i == j)
+                    continue;
+                model.add(T[j] >= T[i] - M * (1 - y[i][j]));
+            }
+        }
+
+        for (int i : N0)
+        {
+            for (int j : D)
+            {
+                if (i == j)
+                    continue;
+                model.add(T[j] >= T[i] + instance->time_truck[i][j] + instance->delta * u[j] - M * (1 - x[i][j]));
+            }
+        }
+
+        for (int i : N0)
+        {
+            for (int j : Ne)
+            {
+                if (i == j)
+                    continue;
+                if (isElementInVector(D, j))
+                    continue;
+                model.add(T[j] >= T[i] + instance->time_truck[i][j] - M * (1 - x[i][j]));
+            }
+        }
+
+        for (int j : D)
+        {
+            model.add(T[j] >= s[j] + (instance->time_drone[0][j] + instance->delta) * z[j] - M * (1 - u[j]));
+        }
+
+        for (int i : D)
+        {
+            for (int j : D)
+            {
+                if (i == j)
+                    continue;
+                model.add(s[j] >= T[i] + instance->time_drone[0][i] - M * (1 - r[i][j]));
+            }
+        }
+
+        for (int i : D)
+        {
+            model.add(T[i] <= M * z[i]);
+        }
+
+        for (int i : D)
+        {
+            for (int j : D)
+            {
+                if (i == j)
+                    continue;
+                model.add(s[j] >= s[i] + 2 * instance->time_drone[0][i] + instance->delta * u[i] - M * (1 - r[i][j]) - M * z[i]);
+            }
+        }
+
+
+        //Constraints on the start time of the truck route and drone ﬂights
+        for (int i : N)
+        {
+            for (int j : D)
+            {
+                model.add(s[j] >= instance->release_time[i] * y[j][i] + instance->release_time[j] * (1 - z[j]));
+            }
+        }
+
+        for (int j : N)
+        {
+            model.add(T[0] >= instance->release_time[j] * y[0][j]);
+        }
+
+        //Computation of wait times and lower bounds for the total delivery time
+        for (int j : D)
+        {
+            for (int i : N0)
+            {
+                if (i == j)
+                    continue;
+                model.add(e[j] >= T[j] - (T[i] + instance->time_truck[i][j]) - M * (1 - x[i][j]));
+            }
+        }
+
+        for (int j : D)
+        {
+            model.add(e[j] <= M * u[j]);
+        }
+
+        for (int i : N)
+        {
+            model.add(T[n - 1] >= T[i] + instance->time_truck[i][n - 1] - M * (1 - z[i]));
+        }
+
+        IloExpr sum_cost(env);
+        sum_cost += T[0];
+        for (int i : N0)
+        {
+            for (int j : Ne)
+            {
+                if (i == j)
+                    continue;
+                sum_cost += instance->time_truck[i][j] * x[i][j];
+            }
+        }
+
+        for (int i : D)
+        {
+            sum_cost += e[i];
+        }
+
+        model.add(T[n - 1] >= sum_cost);
+
+        for (int i : D)
+        {
+            model.add(Td >= s[i] + 2 * instance->time_drone[0][i] + instance->delta * z[i] - M * (1 - r[i][n - 1]));
+        }
+
+        std::cout << "done" << '\n';
+        return model;
+    }
+
+    Solution run() override {
+
+        Param* param = Param::getInstance();
+
+        IloModel model = createModel();
+        IloCplex cplex(model);
+
+        //cplex.setParam(IloCplex::Param::TimeLimit, 10); // Set a time limit if needed
+        cplex.setParam(IloCplex::Param::Threads, 1);
+        //  cplex.setParam(IloCplex::Param::MIP::Tolerances::MIPGap, param->getGap());
+        //cplex.setOut(env.getNullStream());
+
+        auto start_time = std::chrono::high_resolution_clock::now();
+        Solution sol;
+        std::cout << "have solve??" << '\n';
+        if (cplex.solve()) {
+            std::cout << "solve 0" << '\n';
+            auto end_time = std::chrono::high_resolution_clock::now();
+            sol.time = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time).count();
+            std::cout << cplex.getObjValue() << '\n';
+            sol.obj = cplex.getObjValue(); \
+
+
+            std::vector<std::vector<int>> x_sol(n, std::vector<int>(n, 0));
+             for (int i : D)
+             {
+                 if (cplex.getValue(u[i]) >= 0.5)
+                     std::cout << "u[" << i << "] = " << cplex.getValue(u[i]) << std::endl;
+             }
+             std::cout << '\n';
+
+             for (int i : D)
+             {
+                 if (cplex.getValue(e[i]) >= 0.5)
+                     std::cout << "e[" << i << "] = " << cplex.getValue(e[i]) << std::endl;
+             }
+             std::cout << '\n';
+
+             for (int i : Na)
+             {
+                 std::cout << "T[" << i << "] = " << cplex.getValue(T[i]) << std::endl;
+             }
+            float sum = cplex.getValue(T[0]);
+            std::cout << "sum = " << sum << "\n";
+            std::vector<int> tour;
+            int current = 0;   // start from point 0
+
+            for (int i : N0)
+            {
+                for (int j : Ne)
+                {
+                    if (i == j) continue;
+                    if (cplex.getValue(x[i][j]) >= 0.5)
+                    {
+                        //sum += time_truck[i][j];
+                        //std::cout << "time_truck[" << i << "][" << j << "] = " << time_truck[i][j] << '\n';
+                        std::cout << i << " " << j << std::endl;
+                        x_sol[i][j] = 1;
+                    }
+
+                }
+            }
+
+            tour.push_back(current);
+
+            while (true) {
+                int next_point = -1;
+                for (int j = 0; j < n; j++) {
+                    if (x_sol[current][j] == 1) {  // if there's a tour from current to j
+                        next_point = j;
+                        break;
+                    }
+                }
+
+                if (next_point == -1) break;  // no next point, end the tour
+
+                tour.push_back(next_point);
+                current = next_point;  // move to the next point
+
+                if (current == n - 1) break;  // if we've reached the last point, stop
+            }
+
+            // Output the tour
+            std::cout << "Truck tour: ";
+            for (int node : tour) {
+                if (node == n - 1)
+                    std::cout << 0 << " ";
+                else
+                    std::cout << node << " ";
+            }
+            std::cout << std::endl;
+            std::cout << "Drone tour: ";
+            for (int i : D0)
+            {
+                for (int j : De)
+                {
+                    if (i == j) continue;
+                    if (cplex.getValue(r[i][j]) >= 0.5)
+                    {
+                        //sum += time_truck[i][j];
+                        //std::cout << "time_truck[" << i << "][" << j << "] = " << time_truck[i][j] << '\n';
+                        std::cout << "r[" << i << "][" << j << "] = " << cplex.getValue(r[i][j]) << std::endl;
+                        
+                    }
+
+                }
+            }
+            if (cplex.getStatus() == IloAlgorithm::Optimal) {
+                sol.status = "Optimal";
+                std::cout << "optimal" << '\n';
+                std::cout << cplex.getObjValue() << '\n';
+                sol.obj = cplex.getObjValue(); \
+
+
+                
+            }
+            else if (cplex.getStatus() == IloAlgorithm::Feasible) {
+
+                sol.status = "Feasible";
+               /* for (int i = 0; i < s.getSize(); i++)
+                    sol.st.push_back(cplex.getValue(s[i]));
+                sol.obj = cplex.getObjValue();*/
+            }
+            else {
+
+                sol.status = "Infeasible";
+                sol.obj = -1;
+            }
+        }
+        else {
+
+            auto end_time = std::chrono::high_resolution_clock::now();
+            sol.time = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time).count();
+            sol.status = "NoSolution";
+        }
+
+        return sol;
+    }
+
+
+    int isElementInVector(const std::vector<int>& vec, int element) {
+        for (int i = 0; i < vec.size(); i++) {
+            if (vec[i] == element) {
+                return 1;  // Element found
+            }
+        }
+        return 0;  // Element not found
+    }
+};
